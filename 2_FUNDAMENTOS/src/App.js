ba4e11430b48de7e2b2442571444a7ad8a604e1a
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2024-01-11 15:28:24 -0300

    event button

--- 2_FUNDAMENTOS/src/App.js
+++ 2_FUNDAMENTOS/src/App.js
@@ -1,7 +1,9 @@
 import FirstComponent from "./components/FirstComponent";
 import TemplateExpressions from "./components/TemplateExpressions";
+import Events from "./components/Events";
 
 import "./App.css";
+
 
 
 function App(){
@@ -10,6 +12,7 @@ function App(){
       <h1>Fundamentos React</h1>
       <FirstComponent/>
       <TemplateExpressions/>
+      <Events/>
     </div>
   );
 }
---
+++ 2_FUNDAMENTOS/src/components/Events.js
@@ -0,0 +1,15 @@
+const Events = () => {
+
+    const handleMyEvent = () => {
+
+    }
+
+    return (<div>
+        <div>
+            <button onClick={handleMyEvent()}>Clique Event!</button>
+        </div>
+    </div>
+    );
+};
+
+export default Events;
