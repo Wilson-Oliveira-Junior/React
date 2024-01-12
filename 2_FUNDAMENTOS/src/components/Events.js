a1dd9caa8f3a966501c57e360f43a1b26bc41161
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2024-01-12 08:51:50 -0300

    Update Events.js

--- 2_FUNDAMENTOS/src/components/Events.js
+++ 2_FUNDAMENTOS/src/components/Events.js
@@ -4,9 +4,16 @@ const Events = () => {
 
     }
 
-    return (<div>
+    return (
+    <div>
         <div>
             <button onClick={handleMyEvent()}>Clique Event!</button>
+        </div>
+        <div>
+            <button onClick={() => console.log("Clicou!")}>Clique aqui também!</button>
+            <button onClick={() => {if(true){
+                console.log("Isso não deveria aparecer");
+            }}}>Clica aqui por favor</button>
         </div>
     </div>
     );
