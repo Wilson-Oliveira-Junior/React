ef62b9443bd524c509c934ab77614b946ca31d16
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2024-01-12 14:04:23 -0300

    lesson3

---
+++ 3_AVANCANDO_NO_REACT/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <h1>Avançado em React</h1>    
+    <div>
+    <img src="./img1.jpg" alt="Paisagem" />
+    </div>
+    </div>
+  )
+}
+
+export default App
