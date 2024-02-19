eb2120f5208c6e337a4c3b4418a5505ec80a6bb0
Wilson Oliveira Junior <114811978+Wilson-Oliveira-Junior@users.noreply.github.com>
2024-02-19 11:39:47 -0300

    lists

--- 3_AVANCANDO_NO_REACT/src/App.js
+++ 3_AVANCANDO_NO_REACT/src/App.js
@@ -1,4 +1,6 @@
 import ListRender from "./components/ListRender"
+import CarDetails from "./components/CarDetails"
+
 
 function App() {
 
@@ -10,6 +12,10 @@ function App() {
     <img src="./img1.jpg" alt="Paisagem" />
     </div>
     <ListRender/>
+    <CarDetails/>
+    <CarDetails brand="VW" km={100000} color="Azul" newCar={false}/>
+    <CarDetails brand="Ford" color="Vermelha" km={0} newCar={true}/>
+    <CarDetails brand="Fiat" color="Branco" km={4500} newCar={false}/>
     </div>
   )
 }
