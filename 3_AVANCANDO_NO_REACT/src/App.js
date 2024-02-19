8c5cad212f41c664bea77d5d6b4cf839cf68dc4c
Wilson Oliveira Junior <114811978+Wilson-Oliveira-Junior@users.noreply.github.com>
2024-02-19 11:51:50 -0300

    reindenização de arrays

--- 3_AVANCANDO_NO_REACT/src/App.js
+++ 3_AVANCANDO_NO_REACT/src/App.js
@@ -4,7 +4,13 @@ import CarDetails from "./components/CarDetails"
 
 function App() {
 
-  
+  const cars =[
+    {id: 1, brand: "Ferrari", color :"Amarela", newCar:true, km: 0},
+    {id: 2, brand: "KIA", color :"Branca", newCar:false, km: 234},
+    {id: 3, brand: "Renault", color :"Azul", newCar:false, km: 334554},
+  ];
+
+
   return (
     <div className="App">
       <h1>Avançado em React</h1>    
@@ -16,8 +22,15 @@ function App() {
     <CarDetails brand="VW" km={100000} color="Azul" newCar={false}/>
     <CarDetails brand="Ford" color="Vermelha" km={0} newCar={true}/>
     <CarDetails brand="Fiat" color="Branco" km={4500} newCar={false}/>
+    {cars.map((car) => (
+      <CarDetails
+      brand={car.brand}
+      color={car.color}
+      km={car.km}
+      newCar={car.newCar}/>
+      ))}
     </div>
-  )
+  );
 }
 
 export default App
